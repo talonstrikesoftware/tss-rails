7187a62e676b60206942e70a12386dcd91508dc9
Mark <support@talonstrikesoftware.com>
2019-08-18 23:51:23 +0000

    configured sed command for linux

--- project-setup.sh
+++ project-setup.sh
@@ -4,7 +4,7 @@ cp env.template .env
 cp ./rails/rails.env.template ./rails/rails.env
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-   sed -i '' -e "s/PROJECT_DIRECTORY_NAME/$1/g"  $PWD/.env
+   sed -i -e "s/PROJECT_DIRECTORY_NAME/$1/g"  $PWD/.env
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # have to do it this way because of mac OSX, will need to adjust for different OS's
     sed -i '' -e "s/PROJECT_DIRECTORY_NAME/$1/g"  $PWD/.env
